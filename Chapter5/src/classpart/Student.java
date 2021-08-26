e107c5f9b511f6f4ee15bd07cc0d80d59e62691e
juyeon <ljy9311@naver.com>
2021-08-27 01:02:19 +0900

    0827

---
+++ Chapter5/src/classpart/Student.java
@@ -0,0 +1,16 @@
+package classpart;
+
+public class Student {
+
+	int studentID;
+	String studentName;
+	String address;
+
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+}
---
+++ Chapter5/src/classpart/StudentTest.java
@@ -0,0 +1,12 @@
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student studentLee = new Student();
+		studentLee.studentName = "이주연";
+		studentLee.address = "서울";
+
+		studentLee.showStudentInfo();
+	}
+}
